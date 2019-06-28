afd1465cb0744dfc6c900125414e81179bd1c627
Juliana Chois <julianachois@Julianas-MBP.localdomain>
2019-06-28 10:15:08 -0500

    test

--- date.go
+++ date.go
@@ -7,23 +7,86 @@ import (
 	// age "github.com/bearbin/go-age"
 )
 
-func main()  {
+type User struct {
+	FirstName string
+	LastName  string
+	Birthdate  time.Time
+	// ExpectedAge int
+}
 
-	// var birth time.Time
+var firtName, lastName, birthdate string
 
-	// fmt.Println("-------------")
-	// fmt.Scanf("%v \n", birth)
+func info() string {
+	fmt.Println("\n")
+	fmt.Println("Create a new user: \n")
 
-	// layout := "2006-01-02" //T15:04:05.000Z
-	// str := birth
+	fmt.Println("Firt Name:")
+	fmt.Scanf("%v \n", &firtName)
+	fmt.Println("Last Name:")
+	fmt.Scanf("%v \n", &lastName)
+	fmt.Println("Birthdate: (dd/mm/yyyy)")
+	fmt.Scanf("%v \n", &birthdate)
+
+	return fn
+}
+
+func process(firtName string) User {
+	currentDate := time.Now()
+
+	layout := "02-01-2006"
+	str := birthdate
+	t, err := time.Parse(layout, str)
+	
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	// age := age.AgeAt(t, currentDate)
+
+	u := &User{
+		FirstName: firtName,
+		LastName: lastName,
+		Birthdate: t,
+		// ExpectedAge: age,
+	}
+
+	return user
+}
+
+func print()  {
+	// age := process(age)
+
+	fmt.Println("\nJejejejeej: $$$$")
+}
+
+func main() {
+	// currentDate := time.Now()
+
+	fn:=info()
+	u:=print(fn)
+
+	// layout := "02-01-2006"
+	// str := birthdate
 	// t, err := time.Parse(layout, str)
-
+	
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
-	// fmt.Printf("%s\n",t)
 
-	dt := time.Now()
-    //Format MM-DD-YYYY
-    fmt.Println(dt)
+	// age := age.AgeAt(t, currentDate)
+
+	// u := &User{
+	// 	FirstName: firtName,
+	// 	LastName: lastName,
+	// 	Birthdate: t,
+	// 	ExpectedAge: age,
+	// }
+
+	// date := u.Birthdate
+	
+	// fmt.Println("\nName: ", process(u.FirstName))
+	// fmt.Println("Birthdate: ", date.Format("Jan 02"))
+	// fmt.Println("Age: ", u.ExpectedAge)
+	fmt.Println("\n -------------->>", u)
+
 }
